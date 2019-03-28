25547b5c111c8eeac7af1c56856206fdaedea6cf
Robyn <rbynfnch@msn.com>
2019-03-27 21:35:59 -0600

    add firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,10 +1,21 @@
-const queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y&plot=short&apikey=d87f932b"
+const queryURL =
+  "https://www.omdbapi.com/?t=" + userInput + "&y&plot=short&apikey=d87f932b";
 
 var userInput;
 $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
 });
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCheLluXdaOQFsDgzUCAoIYa-fXdeorWaI",
+  authDomain: "raj-movie-picker.firebaseapp.com",
+  databaseURL: "https://raj-movie-picker.firebaseio.com",
+  projectId: "raj-movie-picker",
+  storageBucket: "raj-movie-picker.appspot.com",
+  messagingSenderId: "18109117603"
+};
+firebase.initializeApp(config);
